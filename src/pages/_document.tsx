af2df360f4db4f286c42b9ea9d38ba971d2b5765
sm7f <herberthamorimm@gmail.com>
2025-01-07 21:42:25 -0300

    .

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,14 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
-        {/* Script do Google AdSense */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9408883643416842"
-          crossOrigin="anonymous"
-        ></script>
+        <meta name="google-adsense-account" content="ca-pub-9408883643416842" />
       </Head>
       <body>
         <Main />
